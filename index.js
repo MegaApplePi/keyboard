0708d74f98babd7297367bbcbb99d819f4664733
Eric <123MegaApple123@gmail.com>
2017-03-22 15:23:39 -0500

    fix config, add icons, add icons options,

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 $(document).ready(function(){
  "use strict";
 
+ $('#config_toggle').on("click", function(){
+  if( $('#config').attr("data-state") === "opened" ){
+   $('#config').attr("data-state", "closed");
+   $('#config_toggle > i').empty().append("arrow_back");
+  }else{
+   $('#config').attr("data-state", "opened");
+   $('#config_toggle > i').empty().append("arrow_forward");
+  }
+ });
+
+ $('#keyIconsToggle').on("click", function(){
+  if( $('#keyIconsToggle').prop("checked") ){
+   $('kbd > i.material-icons').addClass("hidden");
+  }else{
+   $('kbd > i.material-icons').removeClass("hidden");
+  }
+ });
+
  $('#keyStyle input').on("click", function(){
   $('#container').attr("data-keyStyle", $(this).val());
  });
@@ -17,6 +35,7 @@ $(document).ready(function(){
  $('#colourInput').on("keydown", function(e){
   if( e.keyCode === 13 ){
    $('#colourPicker').val("#" + $(this).val());
+   $(this).blur();
   }
  });
  $('#colourReset').on("click", function(e){
@@ -41,7 +60,7 @@ $(document).ready(function(){
  });
 
  $('kbd').on("click", function(e){
-  if( $('#colourToggle').prop("checked") ){
+  if( $('#colourToggle_Mouse').prop("checked") ){
    $(e.target).css("background", $('#colourPicker').val());
   }
  });
@@ -51,47 +70,52 @@ $(document).ready(function(){
  });
 
  var keyMap ={};
- //$(document)
- // .on("keydown", function(e){
- // if( !keyMap[e.keyCode] || keyMap[e.keyCode] === undefined ){
- //
- //  /*if(event.ctrlKey){
- //      if(event.ctrlLeft){
- //
- //       console.log('ctrl-left'); 
- //      }else{
- //       console.log('ctrl-right');
- //      }
- //     }else if(event.altKey){
- //      if(event.altLeft){
- //       console.log('alt-left'); 
- //      }else{
- //       console.log('alt-right');
- //      }
- //     }else if(event.shiftKey){
- //      if(event.shiftLeft){
- //       console.log('shift-left'); 
- //      }else{
- //       console.log('shift-right');
- //      }
- //     }else{*/
- //  keyMap[e.keyCode] = true;
- //  if( $("kbd[data-keycode=\"" + e.keyCode + "\"]").attr("data-isPressed") === "true" ){
- //   $("kbd[data-keycode=\"" + e.keyCode + "\"]").attr("data-isPressed", "false");
- //  }else{
- //   $("kbd[data-keycode=\"" + e.keyCode + "\"]").attr("data-isPressed", true);
- //  }
- //  //}
- // }
- // event.preventDefault();
- // event.stopPropagation();
- // return false;
- //})
- // .on("keyup", function(e){
- // if( keyMap[e.keyCode] ){
- //  keyMap[e.keyCode] = false;
- // }
- //});
+ $(document)
+  .on("keydown", function(e){
+  if( $('#colourToggle_Keyboard').prop("checked") && !$("input").is(":focus") ){
+   if( !keyMap[e.keyCode] || keyMap[e.keyCode] === undefined ){
+    if(event.ctrlKey){
+     if(event.ctrlLeft){
+
+      console.log('ctrl-left'); 
+     }else{
+      console.log('ctrl-right');
+     }
+    }else if(event.altKey){
+     if(event.altLeft){
+      console.log('alt-left'); 
+     }else{
+      console.log('alt-right');
+     }
+    }else if(event.shiftKey){
+     if(event.shiftLeft){
+      console.log('shift-left'); 
+     }else{
+      console.log('shift-right');
+     }
+    }else{
+     keyMap[e.keyCode] = true;
+     if( $('kbd[data-keycode="' + e.keyCode + '"]').attr("data-isPressed") === "true" ){
+      $('kbd[data-keycode="' + e.keyCode + '"]').attr("data-isPressed", "false");
+      $('kbd[data-keycode="' + e.keyCode + '"]').css("background", $('#colourPicker').val());
+     }else{
+      $('kbd[data-keycode="' + e.keyCode + '"]').attr("data-isPressed", true);
+      $('kbd[data-keycode="' + e.keyCode + '"]').css("background", "");
+     }
+    }
+   }
+   event.preventDefault();
+   event.stopPropagation();
+   return false;
+  }
+ })
+  .on("keyup", function(e){
+  if( $('#colourToggle_Keyboard').prop("checked") && !$("input").is(":focus") ){
+   if( keyMap[e.keyCode] ){
+    keyMap[e.keyCode] = false;
+   }
+  }
+ });
 
  $(window).on("contextmenu", function(e){
   return false;
