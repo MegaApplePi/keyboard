382e4748fce12d0ce0f3630b15d1e62e927803a2
Eric <123MegaApple123@gmail.com>
2017-09-30 17:26:51 -0500

    add "mode" toggle in options panel and the ability to add notes
    add GitHub repo link

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@
   "use strict";
   const $ = Sizzle;
 
-  // capture key events and prevent their default actions
+  /*// capture key events and prevent their default actions
   let map ={};
   document.addEventListener('keydown', function(e){
     e.preventDefault();
@@ -19,7 +19,7 @@
       delete map[key];
     }
     return false;
-  });
+  });*/
 
   // don't allow the context menu to appear
   window.addEventListener('contextmenu', function(e){
@@ -29,18 +29,47 @@
 
   // define DOM elements
   const $keyboard = $('#keyboard')[0];
+  const $modeToggle = $('#mode-toggle')[0];
   const $colourSelection = $('#colour-selection')[0];
   const $alignmentSelection = $('#alignment-selection')[0];
   const $savePNG = $('#savePNG')[0];
   const $saveJPG = $('#saveJPG')[0];
 
+  let isColourModeEnabled = false;
   let colour = "white";
+
+  $modeToggle.addEventListener('click', function(e){
+    let {target} = e;
+    let $colourSelection_inputs = $('#colour-selection input');
+    let $kbd_inputs = $('#keyboard kbd input');
+    if( target.tagName === "INPUT" ){
+      if( target.value === "note" ){
+        isColourModeEnabled = false;
+        $('#colour-selection input').forEach(function(e){
+          e.setAttribute('disabled', "");
+        });
+        $kbd_inputs.forEach(function(e){
+          e.removeAttribute('disabled');
+        });
+      }else{
+        isColourModeEnabled = true;
+        $colourSelection_inputs.forEach(function(e){
+          e.removeAttribute('disabled');
+        });
+        $kbd_inputs.forEach(function(e){
+          e.setAttribute('disabled', "");
+        });
+      }
+    }
+  });
 
   $keyboard.addEventListener('click', function(e){
     let {target} = e;
     if( target.tagName === "KBD" ){
-      if( colour !== "white" ){
-        target.setAttribute('data-colour', colour);
+      if( isColourModeEnabled ){
+        if( colour !== "white" ){
+          target.setAttribute('data-colour', colour);
+        }
       }
     }
   });
